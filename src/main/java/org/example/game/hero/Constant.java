3bc3328c916303246fef2d8555611f440fad547c
AnaBraznikova <a-brazanikova93@rambler.ru>
2022-11-24 21:01:23 +0100

    My first homework

---
+++ src/main/java/org/example/game/hero/Constant.java
@@ -0,0 +1,8 @@
+package org.example.game.hero;
+
+public class Constant {
+
+    public static final int ELF_POWER = 100;
+    public static final boolean ELF_CHARISMA = true;
+    public static final int ELF_MAGIC = 100;
+}
---
+++ src/main/java/org/example/game/hero/Elf.java
@@ -0,0 +1,60 @@
+package org.example.game.hero;
+
+public class Elf {
+
+    private String id;
+    private String name;
+    private int age;
+    private boolean magic;
+    private double weight;
+    private int money;
+
+
+
+
+
+// getter
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private boolean isMagic() {
+        return magic;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+
+
+
+
+
+
+// setter
+    public Elf(String id, String name, int age, boolean magic, double weight, int money) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.magic = magic;
+        this.weight = weight;
+        this.money = money;
+    }
+
+
+
+
+}
